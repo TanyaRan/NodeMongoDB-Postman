90b6f20093c8e38ec13089149ebdc54312f20c9f
TanyaRan <tatyanaran@gmail.com>
2021-03-27 12:22:58 +0200

    error hndling, mapgeocoder

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please add a name'],
-    // unique: true,
+    unique: true,
     trim: true,
     maxLength: [50, 'Name cannot be more than 50 characters']
   },
@@ -37,25 +38,33 @@ const BootcampSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please add an address']
   },
-  // location: {
-  //   // GeoJSON point
-  //   type: {
-  //     type: String,
-  //     enum: ['Point'],
-  //     required: true
-  //   },
-  //   coordinates: {
-  //     type: [Number],
-  //     required: true,
-  //     index: '2dsphere'
-  //   },
-  //   formattedAddress: String,
-  //   street: String,
-  //   city: String,
-  //   state: String,
-  //   zipcode: String,
-  //   country: String
-  // },
+  location: {
+    // GeoJSON point
+    type: {
+      type: String
+      // enum: ['Point'],
+      // required: true
+    },
+    // latitude: {
+    //   type: Number,
+    //   index: '2dsphere'
+    // },
+    // longitude: {
+    //   type: Number,
+    //   index: '2dsphere'
+    // },
+    coordinates: {
+      type: [Number],
+      // required: true,
+      index: '2dsphere'
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String
+  },
   careers: {
     // Array of strings
     type: [String],
@@ -104,6 +113,25 @@ const BootcampSchema = new mongoose.Schema({
 // Create bootcamp slug from the name
 BootcampSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Geocode & create location field
+BootcampSchema.pre('save', async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].latitude, loc[0].longitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode
+  };
+
+  // Do not save address in DB
+  this.address = undefined;
   next();
 });
 
