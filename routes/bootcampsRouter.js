078c549bb9f7b6c4b07a2e98333c11963f1842ef
TanyaRan <tatyanaran@gmail.com>
2021-03-30 13:31:25 +0300

    Create middleware to filter, select, sort, paging. Implement in bootcamps and courses

--- routes/bootcampsRouter.js
+++ routes/bootcampsRouter.js
@@ -8,6 +8,8 @@ const {
   getBootcampsInRadius,
   bootcampPhotoUpload
 } = require('../controllers/bootcampsCtrl');
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
 // Include other resource router
 const courseRouter = require('./coursesRouter');
 
@@ -20,7 +22,10 @@ router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 router.route('/:id/photo').put(bootcampPhotoUpload);
 
-router.route('/').get(getBootcamps).post(createBootcamp);
+router
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(createBootcamp);
 
 router
   .route('/:id')
